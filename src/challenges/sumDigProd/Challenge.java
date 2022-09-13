2bf4cde80e24c85c0a9d6b8b7768bc8ad25e93da
anOsuPlayer <pelleale261@gmail.com>
2022-09-13 11:28:52 +0200

    added new challenge

--- src/challenges/sumDigProd/Challenge.java
+++ src/challenges/sumDigProd/Challenge.java
@@ -2,11 +2,30 @@ package challenges.sumDigProd;
 
 public class Challenge {
 
-	public static long sumDigProd() {
-		return 0;
+	private static int order(long number) {
+		int order = 0;
+		for (int i = 1; number / i > 0; i *= 10) {
+			order++;
+		}
+		return order;
+	}
+	
+	public static long sumDigProd(long...nums) {
+		long[] def = new long[]{ 0, 0 };
+		for (int i = 0; i < nums.length; i++) {
+			def[0] += nums[i];
+		}
+		while (order(def[0]) > 1) {
+			def[1] = 1;
+			for (int i = 1; i <= order(def[0]); i++) {
+				def[1] *= (int) ((def[0] % Math.pow(10, i)) / Math.pow(10, i-1));
+			}
+			def[0] = def[1];
+		}
+		return def[0];
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println(sumDigProd(16, 28));
 	}
 }
