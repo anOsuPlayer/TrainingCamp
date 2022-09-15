6d34daaa8361cf6d07979cdcdb55bde3ba2eda61
= <pelleale261@gmail.com>
2022-09-16 00:47:57 +0200

    added new stuff

--- c++/oly_try1/korrot.cpp
+++ c++/oly_try1/korrot.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-using std::cin, std::cout, std::endl,
+using std::cin, std::cout, std::endl, std::string,
     std::ofstream, std::ifstream;
 
 int main() {
@@ -21,22 +21,115 @@ int main() {
         }
     }
 
-    int K, P;
-    in >> K >> P;
+    int xK, yK = 0, xP, yP = rows-1;
+    in >> xK >> xP;
 
-    board[1][K-1] = 1;
-    board[rows-1][P-1] = 5;
+    xK--; xP--;
+
+    board[yK][xK] = 1;
+    board[yP][xP] = 5;
 
     int x, y;
-    for (int i = 0; x != EOF; i++) {
+    for (int i = 0; x != 0 && y != 0; i++) {
         in >> x >> y;
-        board[x][y] = 3;
+        board[y-1][x-1] = 3;
     }
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = rows-1; i >= 0; i--) {
         for (int e = 0; e < cols; e++) {
             cout << board[i][e] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
+
+    int tendency = (xP < xK) ? -1 : (xP == xK) ? 0 : 1;
+    string steps; string conclusion("C");
+    bool breaker = true;
+
+    while ((xP != xK || yP != yK) && breaker) {
+        
+        if (yP != yK) {
+            if (board[yK+1][xK] <= 1 || board[yK+1][xK] == 5) {
+                yK++;
+                board[yK][xK] += 1;
+                steps += "A";
+            }
+            else if (board[yK+1][xK] == 3) {
+                if (yK == rows-2) {
+                    if (xK == cols-1) {
+                        xK--;
+                        board[yK][xK] += 1;
+                        steps += "S";
+                    }
+                    else {
+                        if (tendency == 0) {
+                            xK++;
+                            board[yK][xK] += 1;
+                            steps += "D";
+                        }
+                        else {
+                            xK += tendency;
+                            board[yK][xK] += 1;
+                            steps += "D";
+                        }
+                    }
+                }
+                else {
+                    if (xK != cols-1) {
+                        xK++;
+                        board[yK][xK] += 1;
+                        steps += "D";
+                    }
+                    else {
+                        xK--;
+                        board[yK][xK] += 1;
+                        steps += "S";
+                    }
+                }
+            }
+        }
+        else {
+            if (board[yK][xK+tendency] <= 1 || board[yK+1][xK] == 5) {
+                xK += tendency;
+                board[yK][xK] += 1;
+                steps += (tendency == 1) ? "D" : "S";
+            }
+            else if (board[yK][xK+tendency] == 3) {
+                if (yK == rows-1) {
+                    yK--;
+                    board[yK][xK] += 1;
+                    steps += "B";
+                }
+                else {
+                    xK++;
+                    board[yK][xK] += 1;
+                    steps += "D";
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int e = 0; e < cols; e++) {
+                if (board[i][e] == 2) { breaker = false; }
+                conclusion == "C";
+                break;
+            }
+        }
+
+        if (xK == xP && yK == yP) {
+            conclusion == "P";
+        }
+
+        int tendency = (xP < xK) ? -1 : (xP == xK) ? 0 : 1;
+    }
+
+    cout << endl;
+    for (int i = rows-1; i >= 0; i--) {
+        for (int e = 0; e < cols; e++) {
+            cout << board[i][e] << " ";
+        }
+        cout << endl;
+    }
+
+    out << steps << endl << conclusion;
 }
